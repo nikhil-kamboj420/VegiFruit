428653279266420d9b81c4d4e1fba7522930ab32
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2026-03-16 22:10:44 +0530

    define  / route

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use("/",(req,res) => {
+  res.send("Hello, World!");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
